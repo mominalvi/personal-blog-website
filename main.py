3e36333a18b987bece57a281b636d8ab776aa4a0
mominalvi <mominalvi30@gmail.com>
2023-11-16 12:23:43 -0500

    trying to fix postgres db access issue

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ login_manager.init_app(app)
 # CONNECT TO DB
 
 # uri = os.environ.get("DB_URI", "sqlite:///blog.db")
-uri = os.getenv('DB_URI')
+uri = os.environ.get('DB_URI')
 print(uri)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
