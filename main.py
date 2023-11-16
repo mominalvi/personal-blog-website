5b85413880f9bd5836d52f950411e8411596efe4
mominalvi <mominalvi30@gmail.com>
2023-11-16 15:03:03 -0500

    personalize the website

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ login_manager.init_app(app)
 # uri = os.environ.get("DB_URI", "sqlite:///blog.db")
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://blog_database_5e9m_user:nBzf2l7xL3z1sgDpiIbl1QGKMVOAuf9A@dpg-clapcmrmot1c7384dcig-a.oregon-postgres.render.com/blog_database_5e9m'
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -257,4 +257,4 @@ def send_email(name, email, phone, message):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
